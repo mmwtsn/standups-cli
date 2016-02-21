7842f1e4e5d315d2071cbd5677641ce71e8128cc
M. Maxwell Watson <hi@mmwtsn.com>
2016-02-21 10:55:01 -0500

    Read standups from disk
    This is returning a raw ByteString rather than the decoded JSON to avoid
    returning an Either value from this readFile wrapper. That error can be
    handled later in the process when the standups are updated.
    
    It also extracts the temporary file name now that it's referenced twice.

--- src/ReadWrite.hs
+++ src/ReadWrite.hs
@@ -5,6 +5,10 @@ import qualified Data.ByteString.Lazy as BS
 import           GHC.Word
 import           Data
 
+-- Name for temporary file used while standup is being added to
+inProgress :: FilePath
+inProgress = ".in-progress.json"
+
 -- Override aeson-pretty's four-space default value, ignore sort function
 config :: Data.Aeson.Encode.Pretty.Config
 config = Config { confIndent = 2, confCompare = mempty }
@@ -23,4 +27,8 @@ encodeWithNewLine standup = appendNewLine $ encodePretty' config standup
 
 -- Write a JSON-encoded Standup to disk, overwriting the existing file
 writeStandup :: Standup -> IO ()
-writeStandup standup = BS.writeFile ".in-progress.json" $ encodeWithNewLine standup
+writeStandup standup = BS.writeFile inProgress $ encodeWithNewLine standup
+
+-- Read a JSON-encoded standup from disk without parsing it
+readStandup :: IO BS.ByteString
+readStandup = BS.readFile inProgress

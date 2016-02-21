9a9475ac61512b0015de2811061be3810e7fbed0
M. Maxwell Watson <hi@mmwtsn.com>
2016-02-20 20:11:47 -0500

    Create Standup from user input
    This creates a new module for presenting users with formatted command
    line prompts and building new Standup instances with the input:
    
    ghci> createStandup
    Category? $INPUT
    Task?     $INPUT
    
    It adds the ansi-terminal package as a library dependency to provide
    text formatting support.

---
+++ src/Prompt.hs
@@ -0,0 +1,38 @@
+module Prompt where
+
+import System.Console.ANSI
+import Data
+
+-- Default text formatting when user is prompted for input
+promptColor :: System.Console.ANSI.SGR
+promptColor = SetColor Foreground Dull Yellow
+
+-- Reset all text formatting to base Terminal settings
+resetColor :: IO ()
+resetColor = setSGR [Reset]
+
+-- Prompt user for a line of input with a given prompt string and formatting
+getTask :: System.Console.ANSI.SGR -> String -> IO String
+getTask sgr prompt = setSGR [sgr] >>
+                     putStr prompt >>
+                     resetColor >>
+                     getLine
+
+-- Prompt user for a given prompt string using the default formatting
+promptBase :: String -> IO String
+promptBase = getTask promptColor
+
+-- Prompt user for a task category
+promptCategory :: IO String
+promptCategory = promptBase "Category? "
+
+-- Prompt user for a task action
+promptAction :: IO String
+promptAction = promptBase "Task?     "
+
+-- Create a new instance of Standup from user input with a default done Task
+createStandup :: IO ()
+createStandup = do
+  _category <- promptCategory
+  _action <- promptAction
+  print $ Standup [Task _category _action] [Task "attn" "do work"]

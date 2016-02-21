f809a5f19ff9460d27be710707aef442c09b6e4b
M. Maxwell Watson <hi@mmwtsn.com>
2016-02-21 10:46:48 -0500

    Append new line character to end of file
    This allows you to cat the contents of a standup. Without this there is
    no final new line character which leaves the prompt hanging out on the
    last line of the terminal output.

--- src/ReadWrite.hs
+++ src/ReadWrite.hs
@@ -2,8 +2,21 @@ module ReadWrite where
 
 import           Data.Aeson
 import qualified Data.ByteString.Lazy as BS
+import           GHC.Word
 import           Data
+
+-- Decimal value for the ASCII new line character
+asciiNewLine :: GHC.Word.Word8
+asciiNewLine = 10
+
+-- Append a new line character at the end of a ByteString
+appendNewLine :: BS.ByteString -> BS.ByteString
+appendNewLine bytestring = BS.snoc bytestring asciiNewLine
+
+-- Encode a Standup as JSON with a new line character appended at the end
+encodeWithNewLine :: Standup -> BS.ByteString
+encodeWithNewLine standup = appendNewLine $ encode standup
 
 -- Write a JSON-encoded Standup to disk, overwriting the existing file
 writeStandup :: Standup -> IO ()
-writeStandup standup = BS.writeFile ".in-progress.json" $ encode standup
+writeStandup standup = BS.writeFile ".in-progress.json" $ encodeWithNewLine standup

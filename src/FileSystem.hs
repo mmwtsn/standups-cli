ff79ba1b278f872488d85dc15c1a32f45a4242f2
M. Maxwell Watson <hi@mmwtsn.com>
2016-02-21 20:44:06 -0500

    Clarify implementation of basePath
    This breaks down the existing implementation into two smaller functions.

--- src/FileSystem.hs
+++ src/FileSystem.hs
@@ -3,7 +3,10 @@ module FileSystem where
 import System.Directory
 import System.FilePath.Posix
 
+-- Prepend file path to ".standups" archive directory
+prependPath :: FilePath -> FilePath
+prependPath = joinPath . (: [".standups"])
+
 -- Absolute path to the standup's archive directory in "~/.standups"
 basePath :: IO FilePath
-basePath = getHomeDirectory >>= \homeDirectory ->
-  return (joinPath [homeDirectory, ".standups"])
+basePath = getHomeDirectory >>= return . prependPath

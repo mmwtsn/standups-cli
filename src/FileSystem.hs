2d0b20987b64b69cfee48eff6dff847344790777
M. Maxwell Watson <hi@mmwtsn.com>
2016-02-27 08:35:54 -0500

    Create time stamped file name for archive
    This will allow archived standups to be sortable by date as well as have
    an English-readable day of the week appended at the end.

--- src/FileSystem.hs
+++ src/FileSystem.hs
@@ -1,5 +1,6 @@
 module FileSystem where
 
+import Data.Time
 import System.Directory
 import System.FilePath.Posix
 
@@ -10,3 +11,11 @@ prependPath = joinPath . (: [".standups"])
 -- Absolute path to the standup's archive directory in "~/.standups"
 basePath :: IO FilePath
 basePath = getHomeDirectory >>= return . prependPath
+
+-- Format a time stamp like "1947-01-08-Wednesday.json"
+formatTimeStamp :: ZonedTime -> String
+formatTimeStamp = formatTime defaultTimeLocale "%F-%A.json"
+
+-- Build time-stamped file name for today's standup
+currentFileName :: IO String
+currentFileName = getZonedTime >>= return . formatTimeStamp

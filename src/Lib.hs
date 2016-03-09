7274f5e477525eaf091e7805213a0838a7799ff5
M. Maxwell Watson <hi@mmwtsn.com>
2016-03-08 22:58:08 -0500

    Add, export addDone
    This adds another piece of the public-facing API which allows existing
    in-progress standups to be updated. Much of this functionality was
    extracted because I'll soon be adding a very similar addTodo function.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
-    ( createStandup
+    ( addDone
+    , createStandup
     , someFunc
     ) where
 
@@ -17,3 +18,24 @@ createStandup = promptCategory >>= \c ->
                 promptAction   >>= \a ->
                 stashPath      >>= \path ->
                 writeStandup path $ Standup [Task c a] [Task "attn" "do work"]
+
+-- Prompt user for Task and append it to the current standup's "done" Task
+addDone :: IO ()
+addDone = promptCategory >>= \c ->
+          promptAction   >>= \a ->
+          stashPath      >>= \path ->
+          eitherRead c a path appendDone
+
+-- Create a new Task and append it to the end of a Standup's "done" Task
+appendDone :: String -> String -> Standup -> Standup
+appendDone c a standup = Standup (done standup ++ [Task c a]) (todo standup)
+
+-- Attempt to read a JSON-encoded Standup from the file system and either print
+-- an error if unable to decode it or update it with a given update function
+eitherRead :: String -> String -> FilePath
+           -> (String -> String -> Standup -> Standup)
+           -> IO ()
+eitherRead c a path update = readStandup >>= \decoded ->
+                    case decoded of
+                      Left err      -> putStrLn err
+                      Right standup -> writeStandup path $ update c a standup

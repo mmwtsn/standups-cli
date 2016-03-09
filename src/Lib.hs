c93f8d09eeeecfbeea3f369f3afa96c5517fb54c
M. Maxwell Watson <hi@mmwtsn.com>
2016-03-08 23:02:00 -0500

    Add, export addTodo
    This mirrors the update in 7274f5e on the "todo" side of things. In
    order to reduce duplication a bit common functionality is extracted out
    into a updateStandup function.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
     ( addDone
+    , addTodo
     , createStandup
     , someFunc
     ) where
@@ -21,14 +22,26 @@ createStandup = promptCategory >>= \c ->
 
 -- Prompt user for Task and append it to the current standup's "done" Task
 addDone :: IO ()
-addDone = promptCategory >>= \c ->
-          promptAction   >>= \a ->
-          stashPath      >>= \path ->
-          eitherRead c a path appendDone
+addDone = updateStandup appendDone
+
+-- Prompt user for Task and append it to the current standup's "todo" Task
+addTodo :: IO ()
+addTodo = updateStandup appendTodo
+
+-- Prompt user for a Task and attempt to update the in-progress Standup with it
+updateStandup :: (String -> String -> Standup -> Standup) -> IO ()
+updateStandup update = promptCategory >>= \c ->
+                       promptAction   >>= \a ->
+                       stashPath      >>= \path ->
+                       eitherRead c a path update
 
 -- Create a new Task and append it to the end of a Standup's "done" Task
 appendDone :: String -> String -> Standup -> Standup
 appendDone c a standup = Standup (done standup ++ [Task c a]) (todo standup)
+
+-- Create a new Task and append it to the end of a Standup's "todo" Task
+appendTodo :: String -> String -> Standup -> Standup
+appendTodo c a standup = Standup (done standup) (todo standup ++ [Task c a])
 
 -- Attempt to read a JSON-encoded Standup from the file system and either print
 -- an error if unable to decode it or update it with a given update function

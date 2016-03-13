45849e708c5c099cba75e392234628e65e6c3489
M. Maxwell Watson <hi@mmwtsn.com>
2016-03-13 17:07:34 -0400

    Add `archive` subcommand
    This creates an alias for `standup` because I find myself typing this
    more than standup. Not worth deprecating the original subcommand.

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,7 @@ parse ["new"]       = new     >> exit
 parse ["add-done"]  = addDone >> exit
 parse ["add-todo"]  = addTodo >> exit
 parse ["standup"]   = standup >> exit
+parse ["archive"]   = standup >> exit
 parse ["-v"]        = version >> exit
 parse ["--version"] = version >> exit
 parse ["-h"]        = help    >> exit
@@ -32,5 +33,6 @@ help    = putStrLn $
                     , "  add-done    Add a task to the in-progress done"
                     , "  add-todo    Add a task to the in-progress todo"
                     , "  standup     Run `add-todo` then archive the standup"
+                    , "  archive     Alias for `standup`"
                     ]
 exit    = exitWith ExitSuccess

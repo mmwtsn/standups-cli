87e09b0a2b794dfde9e77b9696f2de2c2691a583
M. Maxwell Watson <hi@mmwtsn.com>
2016-03-13 17:04:43 -0400

    Remove colors
    This removes the ansi-terminal package and all rleated functions. While
    it was fun playing around with it initially coloring the prompts
    provided little actual value in the end.

--- src/Prompt.hs
+++ src/Prompt.hs
@@ -1,32 +1,17 @@
 module Prompt where
 
-import System.Console.ANSI
 import System.IO
 
--- Default text formatting when user is prompted for input
-promptColor :: System.Console.ANSI.SGR
-promptColor = SetColor Foreground Dull Yellow
-
--- Reset all text formatting to base Terminal settings
-resetColor :: IO ()
-resetColor = setSGR [Reset]
-
 -- Prompt user for a line of input with a given prompt string and formatting
-getTask :: System.Console.ANSI.SGR -> String -> IO String
-getTask sgr prompt = hSetBuffering stdout NoBuffering >>
-                     setSGR [sgr] >>
+getTask :: String -> IO String
+getTask prompt = hSetBuffering stdout NoBuffering >>
                      putStr prompt >>
-                     resetColor >>
                      getLine
-
--- Prompt user for a given prompt string using the default formatting
-promptBase :: String -> IO String
-promptBase = getTask promptColor
 
 -- Prompt user for a task category
 promptCategory :: IO String
-promptCategory = promptBase "  category: "
+promptCategory = getTask "  category: "
 
 -- Prompt user for a task action
 promptAction :: IO String
-promptAction = promptBase "  task: "
+promptAction = getTask "  task: "
